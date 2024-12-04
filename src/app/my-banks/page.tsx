7c2768552d4a82ef928da5c258c617fd46eab268
mooud <mooudgamer@gmail.com>
2024-12-04 17:12:51 +0330

    Feat: Create other pages + transfer page

--- src/app/my-banks/page.tsx
+++ src/app/my-banks/page.tsx
@@ -1,5 +1,39 @@
+import { getAccountList } from "@/actions/account/getAccountList";
+import { Button } from "@/components/ui/button";
+import { formatAmount } from "@/lib/utils";
+import { Add } from "iconsax-react";
 import React from "react";
 
-export default function MyBanks() {
-  return <div>MyBanksPage</div>;
+export default async function MyBanks() {
+  const accountList = await getAccountList();
+
+  return (
+    <main className="flex flex-col flex-1 gap-5 px-3">
+      <div className="flex justify-between items-center pt-5 ">
+        <h2 className="text-3xl font-bold">Your Banks</h2>
+        <Button className="flex items-center gap-1">
+          Add Bank <Add className="stroke-white" />
+        </Button>
+      </div>
+      <ul className="flex flex-wrap max-md:flex-col">
+        {accountList.length > 0 ? (
+          accountList.map((account, idx) => (
+            <li
+              key={idx}
+              className="flex justify-between gap-5 items-center border rounded-lg p-5">
+              <div className="flex flex-col">
+                <span className="font-semibold text-xl">{account.name}</span>
+                <span className="opacity-70">{account.iban}</span>
+              </div>
+              <span className="font-bold text-lg">
+                {formatAmount(account.balance)}
+              </span>
+            </li>
+          ))
+        ) : (
+          <span>You have not connected any bank accounts.</span>
+        )}
+      </ul>
+    </main>
+  );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import qs from "query-string";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -76,3 +77,30 @@ export function formatAmount(amount: number): string {
 export const removeSpecialCharacters = (value: string) => {
   return value.replace(/[^\w\s]/gi, "");
 };
+
+interface UrlQueryParams {
+  params: string;
+  key: string;
+  value: string;
+}
+export function formUrlQuery({ params, key, value }: UrlQueryParams) {
+  const currentUrl = qs.parse(params);
+
+  currentUrl[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+}
+
+export function encryptId(id: string) {
+  return btoa(id);
+}
+
+export function decryptId(id: string) {
+  return atob(id);
+}
